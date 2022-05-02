01cd24115b222530b57081d9f256182d6a3104dc
Oleksandr Palamarchuk <sashap740@gmail.com>
2022-05-02 18:29:48 +0200

    changes in sign up

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import {Button, Col, Container, Form, Row} from "react-bootstrap";
+import React, {useState} from 'react';
+import {Alert, Button, Col, Container, Form, Row} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const SignUpPage = () => {
 
-    const signUp = () => {
+    const [signUpData, setSignUpData] = useState({
+        email: '',
+        password: '',
+        city: '',
+        address: '',
+        postNumber: '',
+        phoneNumber: ''
+    })
+    const [alerts, setAlerts] = useState([])
 
+    const signUp = (e) => {
+        e.preventDefault()
     }
 
     return (
@@ -18,34 +28,46 @@ const SignUpPage = () => {
                             <Row className="mb-3">
                                 <Form.Group as={Col} controlId="formGridEmail">
                                     <Form.Label>Email</Form.Label>
-                                    <Form.Control type="email" placeholder="Enter email"/>
+                                    <Form.Control type="email" placeholder="Enter email" onChange={
+                                        event => setSignUpData({...signUpData, email: event.target.value})
+                                    }/>
                                 </Form.Group>
 
                                 <Form.Group as={Col} controlId="formGridPassword">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Enter password"/>
+                                    <Form.Control type="password" placeholder="Enter password" onChange={
+                                        event => setSignUpData({...signUpData, password: event.target.value})
+                                    }/>
                                 </Form.Group>
                             </Row>
 
                             <Form.Group className="mb-3" controlId="formGridAddress1">
                                 <Form.Label>City</Form.Label>
-                                <Form.Control placeholder="London"/>
+                                <Form.Control placeholder="London" onChange={
+                                    event => setSignUpData({...signUpData, city: event.target.value})
+                                }/>
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formGridAddress2">
                                 <Form.Label>Address</Form.Label>
-                                <Form.Control placeholder="street, house number, apartment number"/>
+                                <Form.Control placeholder="street, house number, apartment number" onChange={
+                                    event => setSignUpData({...signUpData, address: event.target.value})
+                                }/>
                             </Form.Group>
 
                             <Row className="mb-3">
                                 <Form.Group as={Col} controlId="formGridCity">
                                     <Form.Label>Post number</Form.Label>
-                                    <Form.Control placeholder="number"/>
+                                    <Form.Control placeholder="number" onChange={
+                                        event => setSignUpData({...signUpData, postNumber: event.target.value})
+                                    }/>
                                 </Form.Group>
 
                                 <Form.Group as={Col} controlId="formGridZip">
                                     <Form.Label>Phone number</Form.Label>
-                                    <Form.Control placeholder={'+380XXXXXXXXX'}/>
+                                    <Form.Control placeholder={'+380XXXXXXXXX'} onChange={
+                                        event => setSignUpData({...signUpData, phoneNumber: event.target.value})
+                                    }/>
                                 </Form.Group>
                             </Row>
 
